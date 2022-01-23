241e788800bf5f26e707d3cce8f1408f243ad7cd
mishu85 <miconstantin@yahoo.com>
2022-01-23 11:39:50 +0200

    do cleanup

--- fremp-frontend/src/App.js
+++ fremp-frontend/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Results from "./components/Results";
+import NewName from "./components/NewName";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+function App(props) {
+  // const [state, setState] = useState({name: '', names: [], loading: true });
+
+  const [name, setName] = useState("");
+  const [names, setNames] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    getNames();
+  }, []);
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("name", name);
+    await axios.post("/names", formData);
+    getNames();
+  };
+
+  const handleDeleteAll = async (ev) => {
+    ev.preventDefault();
+    setLoading(true);
+    await axios.delete("/names");
+    getNames();
+  };
+
+  const handleDelete = async (ev) => {
+    ev.preventDefault();
+    setLoading(true);
+    await axios.delete("/names/" + ev.target.id);
+    getNames();
+  };
+
+  // const getNames = () => {
+  //   fetch("/getnames/")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setName("");
+  //       setNames(json);
+  //       setLoading(false);
+  //     });
+  // };
+
+  const getNames = async () => {
+    const response = await axios.get("/names");
+    setName("");
+    setNames(response.data.data);
+    setLoading(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NewName
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          value={name}
+        />
+        <button onClick={handleDeleteAll}>Delete all names</button>
+        {loading ? (
+          <h1>Loading</h1>
+        ) : (
+          <Results names={names} handleDelete={handleDelete} />
+        )}
       </header>
     </div>
   );
 }
-
 export default App;
